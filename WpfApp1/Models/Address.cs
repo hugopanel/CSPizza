39ead7cf3b992ef79cd7665e40d944a2fea90235
Hugo PANEL <hugopanel@gmail.com>
2023-09-30 16:40:24 +0200

    Ajout classe Address #6

---
+++ WpfApp1/Models/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    class Address
+    {
+        /// <summary>
+        /// Street name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Time it takes to deliver an order to that address (in milliseconds).
+        /// </summary>
+        public int DeliveryTime { get; set; }
+    }
+}
